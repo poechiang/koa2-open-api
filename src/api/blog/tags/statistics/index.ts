91f5008126673cc1cfa18bf05ce9314e81fff4b3
poe.chiang <poechiang@live.cn>
2022-04-16 04:21:51 +0800

    add template files to git ignore

--- src/api/blog/tags/statistics/index.ts
+++ src/api/blog/tags/statistics/index.ts
@@ -1,12 +1,24 @@
-import BlogService from "@svr/blog.service";
-import { Context } from "koa";
-import { reverse, sortBy } from "lodash";
+///////////////////////////////////////////////////////////////////////
+//
+// @Author: Jeff
+// @Created at: Apr. 21, 2021 23:02:51
+// @Modified at: Apr. 16, 2022 04:21:15
+// @Modified by: Jeff
+// @一切伟大的行动都始于一个微不足道的开始!
+///////////////////////////////////////////////////////////////////////
+
+import BlogService from '@svr/blog.service';
+import { Context } from 'koa';
+import { reverse, sortBy } from 'lodash';
 
 const service: BlogService = new BlogService();
 
 export const GET = async (ctx: Context) => {
-  const tags = await service.getTagsStatistics();
-  const tagList = Object.entries(tags).map(([tag, count]) => ({ tag, count }));
+    const tags = await service.getTagsStatistics();
+    const tagList = Object.entries(tags).map(([tag, count]) => ({
+        tag,
+        count,
+    }));
 
-  ctx.json(reverse(sortBy(tagList, (o) => [o.count, o.tag])).slice(0, 9));
+    ctx.json(reverse(sortBy(tagList, (o) => [o.count, o.tag])).slice(0, 9));
 };
